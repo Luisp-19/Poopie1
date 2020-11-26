7fbbbebbee8b9006db85871afbceb44942ee514e
Luisp-19 <luispeixoto77@hotmail.com>
2020-11-26 10:35:04 +0000

    Pagina login feita

---
+++ app/src/main/java/com/example/poopie1/api/Builder.kt
@@ -0,0 +1,19 @@
+package com.example.poopie1.api
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Builder {
+    private val client = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://poopie1.000webhostapp.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun<T> buildService(service: Class<T>): T{
+        return retrofit.create(service)
+    }
+}
---
+++ app/src/main/java/com/example/poopie1/api/EndPoints.kt
@@ -0,0 +1,11 @@
+package com.example.poopie1.api
+
+import retrofit2.Call
+import  retrofit2.http.*
+
+/*interface EndPoints{
+
+    @FormUrlEncoded
+    @POST("/api/login/post")
+    fun postTest(@Field("username") username: String, @Field("password") pass: String?): Call<Login>
+}*/
